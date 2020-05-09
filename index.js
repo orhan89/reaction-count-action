01156886e4143dcd469d6aa75d2db855ba2bc64f
Ricky Hariady <ricky@sense-os.nl>
2020-05-10 01:19:58 +0700

    use async function

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const token = core.getInput('token');
-    const reaction = core.getInput('reaction');
-    console.log(`Reaction: ${reaction}!`);
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const token = core.getInput('token');
+        const reaction = core.getInput('reaction');
+        console.log(`Reaction: ${reaction}!`);
 
-    const octokit = new github.GitHub(token);
+        const octokit = new github.GitHub(token);
 
-    const reactions = await octokit.reactions.listForIssue({
-        github.context.owner,
-        github.context.repo,
-        github.context.payload.issue.number,
-        reaction
-    });
+        const reactions = await octokit.reactions.listForIssue({
+            github.context.owner,
+            github.context.repo,
+            github.context.payload.issue.number,
+            reaction
+        });
 
-    core.setOutput("count", reactions.length);
-    
-} catch (error) {
-  core.setFailed(error.message);
+        core.setOutput("count", reactions.length);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
