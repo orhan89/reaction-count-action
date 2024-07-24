3bf1659ac23e40503119bbc48eec8aaa44c62a8a
Ricky Hariady <ricky@sense-os.nl>
2024-07-24 23:34:21 +0700

    clean up unused log

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ const github = require('@actions/github');
 
 async function run() {
     try {
-        // `who-to-greet` input defined in action metadata file
         const token = core.getInput('token');
         const reaction = core.getInput('reaction');
         const issue = github.context.issue;
-
-        console.log(`Reaction: ${reaction}!`);
-        console.log(`Issue: ${issue.owner}!`);
-        console.log(`Issue: ${issue.repo}!`);
-        console.log(`Issue: ${issue.number}!`);
 
         const octokit = new github.GitHub(token);
 
